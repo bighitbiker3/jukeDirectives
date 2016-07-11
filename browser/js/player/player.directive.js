547131341ba36f20b70d507efd617e30d1348799
Elliott McNary <El_Is_Based@MacBook-Pro.local>
2016-07-11 15:29:38 -0400

    up to dynamic

---
+++ browser/js/player/player.directive.js
@@ -0,0 +1,21 @@
+juke.directive('player', function(PlayerFactory){
+  return {
+    restrict: 'E',
+    templateUrl: '/js/player/templates/player.html',
+    link: function(scope, element, attrs){
+      angular.extend(scope, PlayerFactory);
+      scope.toggle = function () {
+        if ( PlayerFactory.isPlaying() ) PlayerFactory.pause();
+        else PlayerFactory.resume();
+      };
+
+      scope.getPercent = function () {
+        return PlayerFactory.getProgress() * 100;
+      };
+
+      scope.handleProgressClick = function (evt) {
+        PlayerFactory.seek(evt.offsetX / evt.currentTarget.scrollWidth);
+      };
+    }
+  }
+})
